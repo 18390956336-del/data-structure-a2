35b3127312a5dc1a96e848b60b350130bf6b4d94
Yucan <18390956336@163.com>
2025-11-19 18:40:46 +0800

    Add files via upload

---
+++ a2/comp2011/a2/DaryHeap_000.java
@@ -0,0 +1,82 @@
+package comp2011.a2; // Don't change this line!
+
+/**
+ *
+ * You are forbidden to use {@code import} or {@code java.} in this file.
+ *
+ * Do not change the signature of any given method, but feel free to introduce new ones, which must be {@code private}.
+ */
+
+/**
+ * VERY IMPORTANT.
+ * 
+ * I've sought help from the following GenAI:
+ * 1.
+ * 2.
+ * 3.
+ * ...
+ * 
+ * I've discussed this question with the following students (secret numbers, not names!):
+ * 1.
+ * 2.
+ * 3.
+ * ...
+ * 
+ * I've sought help from the following Internet resources and books:
+ * 1.
+ * 2.
+ * 3.
+ * ...
+ * 
+ * My secret number is _________, I'm implementing a __-ary ___ (min/max) heap.
+ */
+public class DaryHeap_000<T extends Comparable<T>> { // Please replace 000 with your secret number!
+
+    public DaryHeap_000(int capacity) {
+    }
+
+    public void insert(T x) {
+    }
+
+    // Running time: O( ).
+    public T removeRoot() {
+        return null;
+    }
+
+    // Running time: O( ).
+    private void up(int c) {
+    }
+
+    // Running time: O( ).
+    private void down(int ind) {
+    }
+
+    /**
+     * Merge the given {@code heap} with {@code this}.
+     * The result will be stored in {@code this}.
+     *
+     * VERY IMPORTANT.
+     *
+     * I've sought help from the following GenAI:
+     * 1.
+     * 2.
+     * 3.
+     * ...
+     * 
+     * I've discussed this question with the following students (secret numbers, not names!):
+     * 1.
+     * 2.
+     * 3.
+     * ...
+     * 
+     * I've sought help from the following Internet resources and books:
+     * 1.
+     * 2.
+     * 3.
+     * ...
+     *
+     * Running time: O( ).
+     */
+    public void merge(DaryHeap_000<T> heap) {
+    }
+}
---
+++ a2/comp2011/a2/Tester.java
@@ -0,0 +1,24 @@
+package comp2011.a2;
+
+/*
+ * You are free to use any Java packages here.
+ * Please do NOT submit this file: 5 points deducted if you do.
+ */
+
+public class Tester {
+
+    private static void testTrees() {
+    }
+
+    private static void testHeaps() {
+    }
+    
+    /**
+     * You are free to make any change to this method.
+     * You can even remove it, if you want to test your code with other means.
+     */
+    public static void main(String[] args) {
+        testTrees();
+        testHeaps();
+    }
+}
